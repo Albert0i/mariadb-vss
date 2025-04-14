52defe3e6421fae16b7c46e44ae2c2b2872ec71a
Alberto Iong <albert0i@hotmail.com>
2025-04-14 12:09:04 +0800

    write more

--- src/queryVSS.js
+++ src/queryVSS.js
@@ -69,33 +69,3 @@ askQuestion();
    Finding Relevant Documents
    https://github.com/withcatai/node-llama-cpp/blob/master/docs/guide/embedding.md
 */
-
-/*
-CREATE OR REPLACE FUNCTION cosine_similarity(
-  array1 IN float_array_384,
-  array2 IN float_array_384
-) RETURN BINARY_FLOAT IS
-  dot_product BINARY_FLOAT := 0;
-  norm_a BINARY_FLOAT := 0;
-  norm_b BINARY_FLOAT := 0;
-  similarity BINARY_FLOAT;
-BEGIN
-  IF array1.COUNT != array2.COUNT THEN
-    RAISE_APPLICATION_ERROR(-20001, 'Arrays must be of the same length');
-  END IF;
-
-  FOR i IN 1 .. array1.COUNT LOOP
-    dot_product := dot_product + (array1(i) * array2(i));
-    norm_a := norm_a + (array1(i) * array1(i));
-    norm_b := norm_b + (array2(i) * array2(i));
-  END LOOP;
-
-  IF norm_a = 0 OR norm_b = 0 THEN
-    RETURN 0;
-  ELSE
-    similarity := dot_product / (SQRT(norm_a) * SQRT(norm_b));
-    RETURN similarity;
-  END IF;
-END cosine_similarity;
-
-*/
